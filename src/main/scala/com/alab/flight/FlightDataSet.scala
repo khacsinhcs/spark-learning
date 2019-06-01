be009d40d5ef75579d231d8a66ebe12bfb4454d0
khacsinhcs <khacsinhcs@gmail.com>
2019-06-01 22:34:15 +0700

    refactor Flight data set

--- src/main/scala/com/alab/flight/FlightDataSet.scala
+++ src/main/scala/com/alab/flight/FlightDataSet.scala
@@ -3,16 +3,19 @@ package com.alab.flight
 import com.alab.SparkSessionWrapper
 import org.apache.spark.sql.DataFrame
 
-trait FlightDataSet extends SparkSessionWrapper {
+trait FlightDS extends SparkSessionWrapper {
+  def loadData(): DataFrame
+}
+
+trait FlightCSV extends FlightDS {
   def loadData(): DataFrame = {
     spark.read.option("inferScheme", "true")
       .option("header", "true")
       .csv("data/flight-data/csv/2015-summary.csv")
   }
-
 }
 
-object Flight extends FlightDataSet {
+object Flight extends FlightCSV {
   def main(args: Array[String]): Unit = {
     Flight.loadData()
   }
