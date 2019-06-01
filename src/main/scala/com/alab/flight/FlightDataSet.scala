1aa97fb8f17afc45e769cd80c25320dc38b2e01c
khacsinhcs <khacsinhcs@gmail.com>
2019-06-01 23:07:32 +0700

    test

--- src/main/scala/com/alab/flight/FlightDataSet.scala
+++ src/main/scala/com/alab/flight/FlightDataSet.scala
@@ -1,10 +1,21 @@
 package com.alab.flight
 
 import com.alab.SparkSessionWrapper
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.DataFrame
+
+case class Flight(from: String, des: String, total: Int)
 
 trait FlightDS extends SparkSessionWrapper {
   def loadData(): DataFrame
+}
+
+object FlightRDD {
+
+  implicit class ToRDD(df: DataFrame) {
+    def toRDD: RDD[Flight] = df.rdd.map(row => Flight(row.getString(0), row.getString(1), row.getString(2).toInt))
+  }
+
 }
 
 trait FlightCSV extends FlightDS {
@@ -18,8 +29,11 @@ trait FlightCSV extends FlightDS {
 object FlightTest extends FlightCSV {
   def main(args: Array[String]): Unit = {
     val df: DataFrame = FlightTest.loadData()
-    df.rdd.map(row => Flight(row.getAs[String](0), row.getAs[String](1), row.getString(2).toInt)).take(3).foreach(println(_))
+    df.sort("count").show(30)
+
+    import FlightRDD._
+    val totalFlight = df.toRDD.map(f => f.total).fold(0)((f1, f2) => f1 + f2)
+    println(totalFlight)
   }
 
-  case class Flight(from: String, des: String, total: Int)
 }
