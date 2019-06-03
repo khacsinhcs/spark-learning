89d90e10b0c3b4d4cb646428f78eec09b8d96dfa
khacsinhcs <khacsinhcs@gmail.com>
2019-06-03 14:29:44 +0700

    move to test code to test

--- src/main/scala/com/alab/flight/FlightRepository.scala
+++ src/main/scala/com/alab/flight/FlightRepository.scala
@@ -3,11 +3,11 @@ package com.alab.flight
 import com.alab.{DataFrameLoader, RowMaterialize}
 import org.apache.spark.sql._
 
-case class Flight(from: String, des: String, total: Int)
+case class Flight(from: String, des: String, total: Int) extends Serializable
 
 trait FlightDF extends DataFrameLoader
 
-object RowToFight {
+object FlightMaterialize {
   implicit val flightMaterial: RowMaterialize[Flight] = new RowMaterialize[Flight] {
     override def materialize(row: Row): Flight = Flight(row.getString(0), row.getString(1), row.getString(2).toInt)
   }
@@ -22,7 +22,7 @@ trait FlightCsvDF extends FlightDF {
 trait FlightRepository
   extends FlightDF {
 
-  import RowToFight._
+  import FlightMaterialize._
   import com.alab.DataFrameSyntax._
   import com.alab.MaterializeOps._
   import org.apache.spark.sql.functions._
@@ -47,27 +47,7 @@ object FlightRepository extends FlightRepository with FlightCsvDF
 
 object FlightTest extends FlightCsvDF {
   def main(args: Array[String]): Unit = {
-    val df: DataFrame = FlightTest.loadDataFrame()
-    df.sort("count").show(30)
 
-    df.createOrReplaceTempView("flight_data_2015")
-    val sqlWay = spark.sql(
-      """
-        |SELECT DEST_COUNTRY_NAME, ORIGIN_COUNTRY_NAME
-        |FROM flight_data_2015
-        |ORDER BY count DESC
-      """.stripMargin)
-    sqlWay.show(20)
-    sqlWay.explain()
-
-    df.select("DEST_COUNTRY_NAME", "ORIGIN_COUNTRY_NAME")
-      .orderBy("count")
-      .show(30)
-
-    import RowToFight._
-    import com.alab.DataFrameSyntax._
-    val totalFlight = df.toRDD.map(f => f.total).fold(0)((f1, f2) => f1 + f2)
-    println(totalFlight)
   }
 
 }
--- src/test/scala/com/alab/flight/FlightSpec.scala
+++ src/test/scala/com/alab/flight/FlightSpec.scala
@@ -2,6 +2,7 @@ package com.alab.flight
 
 import com.alab.SparkSessionTestWrapper
 import com.github.mrpowers.spark.fast.tests.DataFrameComparer
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.DataFrame
 import org.scalatest.{Matchers, WordSpec}
 
@@ -31,5 +32,42 @@ class FlightSpec extends WordSpec
       FlightRepositoryTest.ds.filter(flight => flight.total == 20).show(10)
       FlightRepositoryTest.ds.printSchema()
     }
+  }
+
+  "Exploring api" should {
+    import FlightMaterialize._
+    import com.alab.DataFrameSyntax._
+
+    val df: DataFrame = FlightTest.df
+
+    "sort dataframe by count" in {
+      df.sort("count").show(30)
+    }
+
+    "project columns and sort by count by sql" in {
+      df.createOrReplaceTempView("flight_data_2015")
+      val sqlWay = spark.sql(
+        """
+          |SELECT DEST_COUNTRY_NAME, ORIGIN_COUNTRY_NAME
+          |FROM flight_data_2015
+          |ORDER BY count DESC
+        """.stripMargin)
+      sqlWay.show(20)
+      sqlWay.explain()
+    }
+    "project columns and sort by count by code" in {
+      val codeWay = df
+        .select("DEST_COUNTRY_NAME", "ORIGIN_COUNTRY_NAME")
+        .orderBy("count")
+      codeWay.show(20)
+      codeWay.explain()
+    }
+
+    "sum all total flight by RDD" in {
+      val rdd: RDD[Flight] = df.toRDD()
+      val totalFlight = rdd.map(f => f.total).fold(0)(_ + _)
+      println(totalFlight)
+    }
+
   }
 }
